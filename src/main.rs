fdc47e8c5a54248153e0d705adc6ee4e7823cb97
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2024-08-29 01:36:00 -0400

    add code for setting password

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
-use clap::{Parser,ValueEnum};
+use clap::{Parser, ValueEnum};
+use core::panic;
+use rand::{distributions::Alphanumeric, Rng};
+use serde_json::{Map, Value};
 use std::fs::File;
+use std::io::{BufReader, Write};
 use std::path::Path;
-use std::io::BufReader;
 
-#[derive(ValueEnum,Clone,Debug)]
+#[derive(ValueEnum, Clone, Debug)]
 enum Operation {
     Generate,
     Insert,
     Get,
 }
 
-#[derive(ValueEnum,Clone,Debug,Default)]
+#[derive(ValueEnum, Clone, Debug, Default)]
 enum TInterface {
     #[default]
     CLI,
     TUI,
 }
 
-#[derive(Parser,Debug)]
+#[derive(Parser, Debug)]
 #[command(about="A Simple Password Manager", long_about = None)]
 struct Args {
     #[arg(short, long, value_enum)]
@@ -36,31 +39,57 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let path = Path::new("store.json")
+    let path = Path::new("store.json");
     let file = match File::open(&path) {
         Ok(file) => file,
-        Err(why) => panic!("couldn't open store file: {}", why);
+        Err(why) => panic!("couldn't open store file: {}", why),
+    };
+    let reader = BufReader::new(file);
+
+    let mut store: Map<String, Value> = match serde_json::from_reader(reader) {
+        Ok(json) => json,
+        Err(why) => panic!("couldn't parse store file JSON: {}", why),
+    };
+
+    let mut insert_password = |to_insert: String| {
+        store.insert(args.name.clone(), serde_json::Value::String(to_insert));
+
+        let mut new_file = match File::create(&path) {
+            Ok(file) => file,
+            Err(why) => panic!("couldn't update store file: {}", why),
+        };
+
+        match new_file.write_all(Value::Object(store.clone()).to_string().as_bytes()) {
+            Ok(file) => file,
+            Err(why) => panic!("couldn't write to store file: {}", why),
+        }
     };
 
     match args.operation {
         Operation::Generate => {
-            // TODO: generate and store password
-        },
-        Operation::Insert => {
-            // TODO: insert given password
-        },
-        Operation::Get => {
-            let reader = BufReader::new(file);
+            let to_insert = rand::thread_rng()
+                .sample_iter(&Alphanumeric)
+                .take(16)
+                .map(char::from)
+                .collect();
 
-            let store = match serde_json::from_reader(reader) {
-                Ok(json) => json,
-                Err(why) => panic!("couldn't parse store file JSON: {}", why);
+            insert_password(to_insert)
+        }
+        Operation::Insert => {
+            let to_insert = match args.password_to_insert {
+                Some(p) => p,
+                None => {
+                    println!("please pass a password to insert!");
+                    return;
+                }
             };
 
-            match store[args.name] {
-                serde_json::Null => println!("no password for {}", args.name),
-                serde_json::String(s) => println!("password for {}", s),
-            }
+            insert_password(to_insert)
+        }
+        Operation::Get => match &store[&args.name] {
+            Value::Null => println!("no password for {}", args.name),
+            Value::String(s) => println!("password for {}", s),
+            _ => panic!("bad name passed!"),
         },
     }
 }
